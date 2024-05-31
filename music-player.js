816699b17b333532af9f381ffae8d3fa2001675b
SwampJet3077 <swampjet3077@proton.me>
2024-05-31 11:35:25 -0700

    Create music-player.js

---
+++ music-player.js
@@ -0,0 +1,14 @@
+var audio = document.getElementById('audioPlayer');
+
+function playAudio() {
+    audio.play();
+}
+
+function pauseAudio() {
+    audio.pause();
+}
+
+function stopAudio() {
+    audio.pause();
+    audio.currentTime = 0;
+}

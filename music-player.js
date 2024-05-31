c2189e488e96726e23fd904bd3f0612188e0306d
SwampJet3077 <swampjet3077@proton.me>
2024-05-31 12:41:56 -0700

    Update music-player.js

--- music-player.js
+++ music-player.js
@@ -1,14 +1,36 @@
-var audio = document.getElementById('audioPlayer');
+const audioPlayer = document.getElementById('audioPlayer');
+const playlist = document.getElementById('playlist');
+
+// List of songs
+const songs = [
+    { title: 'Song 1', src: 'audio/song1.mp3' },
+    { title: 'Song 2', src: 'audio/song2.mp3' },
+    { title: 'Song 3', src: 'audio/song3.mp3' }
+];
+
+// Load playlist
+songs.forEach((song, index) => {
+    const li = document.createElement('li');
+    li.textContent = song.title;
+    li.dataset.index = index;
+    li.onclick = () => playSong(index);
+    playlist.appendChild(li);
+});
+
+function playSong(index) {
+    audioPlayer.src = songs[index].src;
+    audioPlayer.play();
+}
 
 function playAudio() {
-    audio.play();
+    audioPlayer.play();
 }
 
 function pauseAudio() {
-    audio.pause();
+    audioPlayer.pause();
 }
 
 function stopAudio() {
-    audio.pause();
-    audio.currentTime = 0;
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
 }
